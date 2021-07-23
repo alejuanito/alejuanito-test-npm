559848ddd180749bc19ec6acf234554d56fefa27
Alejandro <alejandro@causayrumbo.pe>
2021-07-23 10:44:21 -0500

    agregando mapeo de logs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-
+const  logger  = require('./logger.service')
 function testMethod(data) {
-    console.log('Se esta probando este dato');
-    console.log(data);
+
+    console.log('Se esta probando la libreria de npm');
+    logger.info(data.message, {
+        summary: {
+          event: data,
+          service: data.service,
+        },
+      });
 }
 
 module.exports = testMethod
---
+++ src/logger.service.js
@@ -0,0 +1,47 @@
+const { createLogger, format, transports } = require('winston')
+const newrelicFormatter = require('@newrelic/winston-enricher')
+
+// set default log level.
+const logLevel = 'info'
+
+const logger = createLogger({
+  level: logLevel,
+  levels: {
+    fatal: 0,
+    crit: 1,
+    warn: 2,
+    info: 3,
+    debug: 4,
+    trace: 5,
+    error: 6
+  },
+  handleExceptions: true,
+  maxsize: 5242880, // 5MB
+  maxFiles: 5,
+  format: format.combine(format.label({ label: 'test' }), newrelicFormatter()),
+  transports: [
+    new transports.Console()
+    // new transports.File({
+    //   filename: 'app.log',
+    // }),
+  ],
+})
+
+logger.stream = {
+  write: function(message, encoding) {
+    // use the 'info' log level so the output will be picked up by both transports (file and console)
+    logger.info(message)
+  },
+}
+
+/* LOGGER EXAMPLES
+  const log = require('./log.js')
+  log.trace('testing')
+  log.debug('testing')
+  log.info('testing')
+  log.warn('testing')
+  log.crit('testing')
+  log.fatal('testing')
+ */
+
+module.exports = logger
